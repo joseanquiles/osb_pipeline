ed1f43390df134a736698d59c6167cf324fdb831
joseanquiles <joseanquiles@gmail.com>
2020-05-21 20:00:21 +0200

    initial commmit

--- bin/main.dart
+++ bin/main.dart
@@ -10,5 +10,12 @@ String readFile({ String filename }) {
 void main(List<String> arguments) {
   final xmlStr = readFile(filename: 'srv-exp-osb-FulfillmentFinanciacionTCF_PPL.pipeline');
   final doc = xml.parse(xmlStr);
-  print(doc.toXmlString(pretty: true, indent: '    '));
+  //print(doc.toXmlString(pretty: true, indent: '    '));
+
+  for (var d in doc.descendants) {
+    print(d.nodeType);
+  }
+  print('*' * 50);
+  print(doc.descendants.length);
+
 }
